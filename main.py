0990d6a546bcc329cf7d8030af7747e8e2cc2c5d
luis-fe <luisengp@gmail.com>
2024-01-23 15:58:04 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
+import os
+from routes import routes_blueprint
+from functools import wraps
+
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 9000))
+app.register_blueprint(routes_blueprint)
+
+CORS(app)
+
+# Decorator para verificar o token fixo
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token == 'custoMPL123':  # Verifica se o token é igual ao token fixo
+            return f(*args, **kwargs)
+        return jsonify({'message': 'Acesso negado'}), 401
+
+    return decorated_function
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
