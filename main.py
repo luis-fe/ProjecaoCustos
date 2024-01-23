c3969b52c3ddad06c3a2a513933283db0e383cad
luis-fe <luisengp@gmail.com>
2024-01-23 17:03:38 -0300

    requeriments.txt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routes import routes_blueprint
 from functools import wraps
 
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 9000))
+port = int(os.environ.get('PORT', 8001))
 app.register_blueprint(routes_blueprint)
 
 CORS(app)
