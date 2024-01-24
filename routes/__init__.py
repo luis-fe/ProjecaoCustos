c530fa882185a4a42f0fef5d0ae597c4784768bf
luis-fe <luisengp@gmail.com>
2024-01-24 08:07:41 -0300

    Api Produtos

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,8 +5,9 @@ routes_blueprint = Blueprint('routes', __name__)
 
 # Importe as rotas dos arquivos individuais
 from .Projecao import Projecao_routes
-
+from .Produtos import Produtos_routes
 
 # Registre as rotas nos blueprints
 routes_blueprint.register_blueprint(Projecao_routes)
+routes_blueprint.register_blueprint(Produtos_routes)
 
