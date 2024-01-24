9e440a9b72a3e73f809102a7f2d0adeadf3703a6
luis-fe <luisengp@gmail.com>
2024-01-24 16:28:33 -0300

    Api Produtos

--- Service/ProjecaoService.py
+++ Service/ProjecaoService.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def ObterProjecoes():
     conn = ConexaoPostgreMPL.conexao()
-    consulta = pd.read_sql('Select * from "Reposicao"."ProjCustos".projecao ',conn)
+    consulta = pd.read_sql('Select distinct projecao from "Reposicao"."ProjCustos".projecao ',conn)
     conn.close()
 
     return consulta
