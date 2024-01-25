92e5246d197324924c78056f889e1824da0f2a40
luis-fe <luisengp@gmail.com>
2024-01-25 10:04:30 -0300

    Api Produtos

--- Service/produtos.py
+++ Service/produtos.py
@@ -129,7 +129,7 @@ def IncrementarProdutos(projecao, empresa):
 
     for p in projecao:
         conn = ConexaoPostgreMPL.conexao()
-        consultaStatus = pd.read_sql('select situacao "Reposicao"."ProjCustos".projecao '
+        consultaStatus = pd.read_sql('select situacao from "Reposicao"."ProjCustos".projecao '
                                      'where nome = %s ',conn, params=(p))
 
         if consultaStatus['situacao'][0] == 'INICIADA':
