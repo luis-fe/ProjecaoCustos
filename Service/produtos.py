4b6e5fe3a8863d23c1a513740412b9285d0d15b0
luis-fe <luisengp@gmail.com>
2024-01-26 08:04:16 -0300

    Api Produtos

--- Service/produtos.py
+++ Service/produtos.py
@@ -208,7 +208,7 @@ def RestricaoEngenharia(engenharia, obs, usuario):
     conn = ConexaoPostgreMPL.conexao()
 
     inserir = 'insert into "Reposicao"."ProjCustos".restricaoengenharia ' \
-              '(codengeharia, obs, usuario) values (%s , %s , %s )'
+              '(codengenharia, obs, usuario) values (%s , %s , %s )'
 
     cursor = conn.cursor()
     cursor.execute(inserir, (engenharia,obs,usuario))
