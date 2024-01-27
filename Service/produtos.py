5867bc32efea8fe80096669c6b5582d2ab50eb8a
luis-fe <luisengp@gmail.com>
2024-01-27 09:41:40 -0300

    Api Produtos

--- Service/produtos.py
+++ Service/produtos.py
@@ -372,7 +372,7 @@ def ConsultaRestricoes(projecao):
     delete = 'delete from "Reposicao"."ProjCustos".produtos ' \
              'where codengenharia in (' \
              'Select codengenharia from "Reposicao"."ProjCustos".restricaoengenharia  where projecao = %s ) ' \
-             ' and where projecao = %s '
+             ' and  projecao = %s '
 
     cursor = conn.cursor()
     cursor.execute(delete,(projecao,projecao,))
